e124f6e26c8ab756a1b26588ddecbf4acb0dd5bf
Marcos4516 <143296750+Marcos4516@users.noreply.github.com>
2023-11-05 14:51:12 -0300

    subir meu site do projeto integrador

---
+++ botao.js
@@ -0,0 +1,6 @@
+<script>
+  document.getElementById('Você Vai Descartar').addEventListener('click', function() {
+    // Use window.location.href para redirecionar para a página desejada
+    window.location.href = '';
+  });
+</script>
